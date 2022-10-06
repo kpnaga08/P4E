869515a3372e27b90f42b9a1ac253136fde55648
Kristina P. Sinaga <107665417+kpnaga08@users.noreply.github.com>
2022-10-06 11:20:40 +0700

    Add files via upload

---
+++ Ch2/Chapter 2 Quiz.py
@@ -0,0 +1,119 @@
+from sqlite3.dbapi2 import _Statement
+
+
+Question 1
+In the following code,
+
+print(98.6)
+
+what is "98.6"?
+
+A conditional Statement
+A constant
+A variable
+An iteration / loop statement 
+
+Answer: A constant
+
+Question 2
+What does the following code print out?
+
+print("123" + "abc")
+123+abc
+123abc 
+This is a syntax error because you cannot add strings
+hello world
+
+Answer: 123abc 
+
+Question 3
+Which of the following is a bad Python variable name?
+
+#spam
+spam_23
+SPAM23
+_spam
+
+Answer: #spam
+
+Question 4
+Which of the following is not a Python reserved word?
+
+spam
+break
+continue
+for
+
+Answer: spam
+
+Question 5
+Assume the variable x has been initialized to an integer value (e.g., x = 3). What does the following statement do?
+
+x = x + 2
+
+This would fail as it is a syntax error
+Create a function called "x" and put the value 2 in the function
+Produce the value "false" because "x" can never equal "x+2"
+
+Retrieve the current value for x, add two to it and put the sum back into x
+
+Answer: 
+Retrieve the current value for x, add two to it and put the sum back into x
+
+Question 6
+Which of the following elements of a mathematical expression in Python is evaluated first?
+
+Subtraction -
+Addition +
+Multiplication *
+Parentheses ( )
+
+Answer: Parentheses ( )
+
+Question 7
+What is the value of the following expression
+
+42 % 10
+
+10
+2
+420
+1042
+
+Answer: 2
+
+Question 8
+What will be the value of x after the following statement executes:
+
+x = 1 + 2 * 3 - 8 / 4
+
+2
+1.0
+3
+5.0
+
+Answer: 5
+
+Question 9
+What will be the value of x when the following statement is executed:
+
+x = int(98.6)
+
+6
+99
+98
+100
+
+Answer: 98
+
+Question 10
+What does the Python input() function do?
+
+
+Pause the program and read data from the user
+Connect to the network and retrieve a web page.
+Read the memory of the running program
+Take a screen shot from an area of the screen
+
+Answer: Pause the program and read data from the user
+
